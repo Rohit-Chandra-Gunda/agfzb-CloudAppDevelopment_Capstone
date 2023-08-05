7a9a84bc9905c09456765f7c3124e6e97ca92b87
Rohit-Chandra-Gunda <grohitchandra@gmail.com>
2023-08-05 10:20:08 -0400

    Add navbar and about us page

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -10,16 +10,12 @@ from datetime import datetime
 import logging
 import json
 
-# Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-
-# Create your views here.
-
-
-# Create an `about` view to render a static about page
-# def about(request):
-# ...
+def about(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/about.html', context)
 
 
 # Create a `contact` view to return a static contact page
